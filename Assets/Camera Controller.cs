97be6606574eef9e96bc990d1672328110bc700a
Pikachu-Gamer8 <atwlav406@gmail.com>
2026-08-20 14:07:13 +1000

    Movement Update Part 2
    Completed fully functional movement system, mouse look, and added functional and aesthetic framework for bat and arm. (You can actually play a game now)(Also I overlooked it but I think for some reason the game skips the main menu screen but I didn't realise before pushing)

---
+++ Assets/Camera Controller.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraController : MonoBehaviour
+{
+    public float sensitivity = 2.0f;
+    private float Xrotation = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+        Xrotation -= mouseY;
+        Xrotation = Mathf.Clamp(Xrotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(Xrotation, 0f, 0f);
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,18 +1,24 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     public Rigidbody rigid;
     public float speed = 7f;
+    public Transform PlayerCamera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigid = this.gameObject.GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 movement = (transform.forward * v + transform.right * h).normalized * speed;
+        rigid.linearVelocity = new Vector3 (movement.x, rigid.linearVelocity.y, movement.z);
     }
 }

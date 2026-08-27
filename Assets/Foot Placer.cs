b179e831602c3b3e6edaa90c28582e77e5370af9
Pikachu-Gamer8 <atwlav406@gmail.com>
2026-08-27 14:15:04 +1000

    Bug Fix Round
    Fixed target and arm desync, added procedural animation capabilities to legs, added script for "walking" procedurally, broken and due for fix.

---
+++ Assets/Foot Placer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FootPlacer : MonoBehaviour
+{
+    public float FootSpacing;
+    public Transform body;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(body.position + (body.right * FootSpacing), Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit info, 10))
+        {transform.position = info.point;}
+    }
+}
